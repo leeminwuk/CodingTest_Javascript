c8bd235de9d77e66114afed18c2c7d4e7ffdfd75
leeminuk <31334541+leeminwuk@users.noreply.github.com>
2024-10-26 22:14:29 +0900

    [level 1] Title: 최소직사각형, Time: 8.28 ms, Memory: 39.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,13 @@
+const solution = (sizes) => {
+
+  const sortedSizes = sizes.map(([w, h]) => w > h ? [w, h] : [h, w]);
+
+  const maxWidth = Math.max(...sortedSizes.map(([w, h]) => w));
+  const maxHeight = Math.max(...sortedSizes.map(([w, h]) => h));
+
+  return maxWidth * maxHeight;
+};
+
+
+
+
