5594290e2141508eca74413b37c14ed1559b7339
leeminuk <31334541+leeminwuk@users.noreply.github.com>
2024-12-06 23:30:05 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 38.81 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,9 @@
+const solution = (strings, n) => {
+    return strings.sort((a, b) => {
+        if (a[n] === b[n]) {
+                    return a.localeCompare(b);
+    
+        }
+            return a[n].localeCompare(b[n]);
+    });
+};
