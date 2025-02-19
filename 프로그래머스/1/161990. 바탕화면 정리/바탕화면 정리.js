f88b11d4c6ac24b6982078050cbe6755325bab9b
leeminuk <31334541+leeminwuk@users.noreply.github.com>
2025-02-19 23:01:43 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,19 @@
+function solution(wallpaper) {
+    
+    let minRow = Infinity;
+    let minCol = Infinity;
+    let maxRow = 0;
+    let maxCol = 0;
+    
+    for (let i = 0; i < wallpaper.length; i++){
+        for(let j = 0; j < wallpaper[i].length; j++){
+            if(wallpaper[i][j] === '#') {
+                minRow = Math.min(minRow, i);
+                minCol = Math.min(minCol, j);
+                maxRow = Math.max(maxRow, i);
+                maxCol = Math.max(maxCol, j);
+            }
+        }
+    }
+  return [minRow, minCol, maxRow + 1, maxCol + 1];
+}
