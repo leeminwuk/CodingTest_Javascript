ee2391346e2c655888699f19ecd6b33b545c6fb3
leeminuk <31334541+leeminwuk@users.noreply.github.com>
2025-10-21 13:53:57 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,7 +1,7 @@
 const solution = (num_list) => {
-    const product = num_list.reduce((acc, cur) => acc * cur, 1);
+    const mul =  num_list.reduce((acc, cur) => acc * cur, 1);
     
     const sum = num_list.reduce((acc,cur) => acc + cur, 0);
     
-    return product < sum * sum ? 1 : 0;
+    return mul < sum * sum ? 1 : 0;
 }
