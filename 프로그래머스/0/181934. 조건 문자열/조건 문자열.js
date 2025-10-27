8bd0bfaf7bf8252f42772d7242c9ff19d3379aa9
leeminuk <31334541+leeminwuk@users.noreply.github.com>
2025-10-27 09:52:49 +0900

    [level 0] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,13 @@
+const solution = (ineq, eq, n, m) => {
+    
+    if( ineq ===  ">" && eq === "="){
+        return n >= m ? 1 : 0;
+    }else if( ineq === ">" && eq === "!"){
+        return n > m ? 1 : 0;
+    }else if (ineq === "<" && eq === "="){
+        return n <= m ? 1 : 0;
+    }else {
+        return n < m ? 1 : 0;
+    }
+    
+}
