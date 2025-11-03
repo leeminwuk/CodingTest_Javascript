9d277cb74bbae6d2c4ecc9379bd9a2b184293eab
leeminuk <31334541+leeminwuk@users.noreply.github.com>
2025-11-03 09:06:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,8 @@
+const solution = (price) => {
+    return Math.floor(
+        price >= 500000 ? price * 0.8:
+        price >= 300000 ? price * 0.9:
+        price >= 100000 ? price * 0.95:
+        price
+    );
+}
