8231f5645c9591c096e80e7101c74c7df3e5fdaf
leeminuk <31334541+leeminwuk@users.noreply.github.com>
2025-11-27 11:33:26 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.25 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,11 @@
+const solution = (arr, queries) => {
+    for (let k = 0; k < queries.length; k++) {
+        const i = queries[k][0];
+        const j = queries[k][1];
+
+        const temp = arr[i];
+        arr[i] = arr[j];    
+        arr[j] = temp;      
+    }
+    return arr;
+}
